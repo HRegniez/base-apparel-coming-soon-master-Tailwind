0d6ac375fbc3b099ebb3ec3d5a38cd41aad4ee73
HRegniez <henriregniez124@gmail.com>
2022-10-07 17:12:35 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        prim1: 'hsl(0, 36%, 70%)',
+        prim11: 'hsl(0, 36%, 70% 0.4)',
+        prim2: 'hsl(0, 93%, 68%)',
+        neut1: 'hsl(0, 6%, 24%)',
+      },
+    },
+  },
+  
+  plugins: [],
+}
